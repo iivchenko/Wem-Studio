cce8484e16e181dd37bf0a2f04955de34b65fc08
Ivan Ivcheno <ivan.ivchenko@citrix.om>
2017-05-25 17:19:40 +0300

    Set general file structure

---
+++ Src/WemManagementStudio.WPF/Entrance.cs
@@ -0,0 +1,16 @@
+﻿namespace WemManagementStudio.Wpf
+{
+    public static class Entrance
+    {
+        /// <summary>
+        /// Application Entry Point.
+        /// </summary>
+        [System.STAThread]
+        public static void Main()
+        {
+            var app = new App();
+            app.InitializeComponent();
+            app.Run();
+        }
+    }
+}
---
+++ Src/WemManagementStudio.WPF/ViewModels/MainViewModel.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+namespace WemManagementStudio.Wpf.ViewModels
+{
+    public sealed class MainViewModel : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        ////private void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        ////{
+        ////    PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        ////}
+    }
+}
