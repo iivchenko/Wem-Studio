808b7ab3372f2692d7a031d80d9254e474b00c26
Ivan Ivcheno <ivan.ivchenko@citrix.om>
2017-05-26 17:14:40 +0300

    Add AddMachineViewModel

---
+++ Src/WemManagementStudio.WPF/ViewModels/AddMachineViewModel.cs
@@ -0,0 +1,139 @@
+﻿using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using System.Windows.Input;
+using WemManagementStudio.Wpf.Common;
+using WemManagementStudio.Wpf.Views;
+
+namespace WemManagementStudio.Wpf.ViewModels
+{
+    public sealed class AddMachineViewModel : IAddMachineViewModel
+    {
+        private string _name;
+        private string _host;
+        private string _login;
+        private string _pass;
+        private MachineType _type;
+
+        public AddMachineViewModel(Settings settings)
+        {
+            AddCommand = new RelayCommand(
+                o =>
+                {
+                    var machine = new Machine
+                    {
+                        Name = DisplayName,
+                        Host = Host,
+                        User = User,
+                        Pass = Pass,
+                        MachineType = Type
+                    };
+
+                    settings.Machines.Add(machine);
+
+                    Serializer.Save(settings);
+                },
+                o =>
+                {
+                    var machine = new Machine
+                    {
+                        Name = DisplayName,
+                        Host = Host,
+                        User = User,
+                        Pass = Pass,
+                        MachineType = Type
+                    };
+
+                    return
+                        !settings.Machines.Contains(machine)
+                        || !string.IsNullOrWhiteSpace(DisplayName)
+                        || !string.IsNullOrWhiteSpace(Host)
+                        || !string.IsNullOrWhiteSpace(User)
+                        || !string.IsNullOrWhiteSpace(Pass);
+                });
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public string DisplayName
+        {
+            get
+            {
+                return _name;
+            }
+
+            set
+            {
+                _name = value;
+
+                OnPropertyChanged();
+            }
+        }
+
+        public string Host
+        {
+            get
+            {
+                return _host;
+            }
+
+            set
+            {
+                _host = value;
+
+                OnPropertyChanged();
+            }
+        }
+
+        public string User
+        {
+            get
+            {
+                return _login;
+            }
+
+            set
+            {
+                _login = value;
+
+                OnPropertyChanged();
+            }
+        }
+
+        public string Pass
+        {
+            get
+            {
+                return _pass;
+            }
+
+            set
+            {
+                _pass = value;
+
+                OnPropertyChanged();
+            }
+        }
+
+        public MachineType Type
+        {
+            get
+            {
+                return _type;
+            }
+
+            set
+            {
+                _type = value;
+
+                OnPropertyChanged();
+            }
+        }
+
+        public ICommand AddCommand { get; set; }
+
+        private void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
---
+++ Src/WemManagementStudio.WPF/Views/IAddMachineViewModel.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel;
+using System.Windows.Input;
+
+namespace WemManagementStudio.Wpf.Views
+{
+    public interface IAddMachineViewModel : INotifyPropertyChanged
+    {
+        string DisplayName { get; set; }
+
+        string Host { get; set; }
+
+        string User { get; set; }
+
+        string Pass { get; set; }
+
+        MachineType Type { get; set; }
+
+        ICommand AddCommand { get; set; }
+    }
+}
